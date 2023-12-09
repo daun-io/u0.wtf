e4ea677e08f37e57f84903d641ed9eb64ca72729
Daun <iam@daun.io>
2023-12-09 17:38:58 +0900

    translate

--- apps/web/emails/invalid-domain.tsx
+++ apps/web/emails/invalid-domain.tsx
@@ -28,7 +28,7 @@ export default function InvalidDomain({
   return (
     <Html>
       <Head />
-      <Preview>Invalid Domain Configuration</Preview>
+      <Preview>잘못된 도메인 구성</Preview>
       <Tailwind>
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-10 max-w-[500px] rounded border border-solid border-gray-200 px-10 py-5">
@@ -37,51 +37,50 @@ export default function InvalidDomain({
                 src={U0_LOGO}
                 width="40"
                 height="40"
-                alt="Dub"
+                alt="U0"
                 className="mx-auto my-0"
               />
             </Section>
             <Heading className="mx-0 my-7 p-0 text-center text-xl font-semibold text-black">
-              Invalid Domain Configuration
+              잘못된 도메인 구성
             </Heading>
             <Text className="text-sm leading-6 text-black">
-              Your domain <code className="text-purple-600">{domain}</code> for
-              your Dub project{" "}
+              U0 프로젝트{" "}
               <Link
                 href={`https://app.u0.wtf/${projectSlug}`}
                 className="font-medium text-blue-600 no-underline"
               >
                 {projectSlug}↗
               </Link>{" "}
-              has been invalid for {invalidDays} days.
+              의 도메인 <code className="text-purple-600">{domain}</code>이(가){" "}
+              {invalidDays}일 동안 잘못 구성되어 있습니다.
             </Text>
             <Text className="text-sm leading-6 text-black">
-              If your domain remains unconfigured for 30 days, it will be
-              automatically deleted from U0. Please click the link below to
-              configure your domain.
+              도메인이 30일 동안 구성되지 않으면 자동으로 U0에서 삭제됩니다.
+              아래 링크를 클릭하여 도메인을 구성하십시오.
             </Text>
             <Section className="my-8 text-center">
               <Link
                 className="rounded-full bg-black px-6 py-3 text-center text-[12px] font-semibold text-white no-underline"
                 href={`https://app.u0.wtf/${projectSlug}/domains`}
               >
-                Configure domain
+                도메인 구성
               </Link>
             </Section>
             <Text className="text-sm leading-6 text-black">
-              If you do not want to keep this domain on Dub, you can{" "}
+              이 도메인을 U0에서 유지하고 싶지 않다면,{" "}
               <Link
                 href={`https://app.u0.wtf/${projectSlug}/domains`}
                 className="font-medium text-blue-600 no-underline"
               >
-                delete it
+                삭제
               </Link>{" "}
-              or simply ignore this email. To respect your inbox,{" "}
+              하거나 이 이메일을 무시하십시오. 유저분의 메일함을 존중하기 위해,{" "}
               {invalidDays < 28
-                ? `we will only send you one more email about this in ${
+                ? `저희는 ${
                     28 - invalidDays
-                  } days.`
-                : "this will be the last time we will email you about this."}
+                  }일 후에 이에 대해 한 번 더 이메일을 보내드릴 것입니다.`
+                : "이것이 이 문제에 대해 우리가 보내드릴 마지막 이메일입니다."}
             </Text>
             <Footer email={email} />
           </Container>
